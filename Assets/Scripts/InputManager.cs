10676e3585d94b06940c5ac478b908ffce6166e8
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-09-18 16:01:13 -0400

    Add basic greening logic

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,15 +7,44 @@ public class InputManager : MonoBehaviour
 {
     InputField inputField;
 
+    private string currentString = "";
+
+    public List<GameObject> textElements;
+
+    string[] myList = new string[] { "test text", "tyui", "opas", "chaeyoungfromtwice" };
+
     void Start()
     {
-        inputField = GetComponent<InputField>();
+        inputField = GetComponentInChildren<InputField>();
         inputField.Select();
         inputField.ActivateInputField();
+        for (int i = 0; i < textElements.Count; i++)
+        {
+            textElements[i].GetComponent<Text>().text = myList[i];
+        }
     }
 
     public void ReadInput(string myStr)
     {
-        Debug.Log(myStr);
+        char lastTyped = myStr[myStr.Length - 1];
+        for (int i = 0; i < textElements.Count; i++)
+        {
+            string currentWord = myList[i];
+            if (currentWord[currentString.Length] == lastTyped)
+            {
+                currentString += lastTyped;
+                string moreGreen = GetStringWithGreenUpTo(currentWord, currentString.Length);
+                textElements[i].GetComponent<Text>().text = moreGreen;
+            }
+        }
+    }
+
+    string GetStringWithGreenUpTo(string input, int max)
+    {
+        string result = "<color=green>";
+        result += input.Substring(0, max);
+        result += "</color>";
+        result += input.Substring(max);
+        return result;
     }
 }
